b4250bf4362c4453f65221c7363d4ce8acba825d
vuteyrin <47200466+vuteyrin@users.noreply.github.com>
2022-01-03 17:34:18 +0700

    first commit

--- component/model/ViewVideo.js
+++ component/model/ViewVideo.js
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import {View,Image,TouchableOpacity,Dimensions, Modal,StyleSheet,Text} from "react-native";
 import { WebView } from 'react-native-webview';
 import { Entypo,Ionicons,EvilIcons,Feather} from '@expo/vector-icons';
-
+import stylemovie from '../../style/movieStyle';
 import {db} from "../../api/firebase";
 import Root from "../root";
 import style from "../../style/style";
@@ -35,13 +35,31 @@ export default function VeiwVideo({data,viewVideo,setViewVideo}) {
         <Root>
          <View style={playStyle.container}>
           <WebView style={playStyle.video} source={{ uri:dataPlay?.url}}/>
-            <View style={playStyle.conTitle}>
-             <TouchableOpacity onPress={() => setViewVideo(!viewVideo)}>
-              <View style={playStyle.btnPlay}>
-               <Text style={playStyle.btnPlayText}>back</Text>
-              </View>
-             </TouchableOpacity>
-           </View>
+           <View style={stylemovie.body}>
+            <View style={stylemovie.conbodycart}>
+                 {products.map((item,index) =>{
+                   return(
+                    <TouchableOpacity key={index} activeOpacity={0.9} onPress={()=> handleGetData(item)}>
+                     <View style={stylemovie.bodycart}>
+                      <Image  style={stylemovie.imgCart} source={{uri: item.image}}/>
+                       <View style={stylemovie.conTitle}>
+                        <View style={stylemovie.renting}>
+                         <Entypo name="star" size={20} color="orange" />
+                         <Entypo name="star" size={20} color="orange" />
+                         <Entypo name="star" size={20} color="orange" />
+                         <Entypo name="star" size={20} color="orange" />
+                         <Entypo name="star-outlined" size={20} color="#FFFF" />
+                        </View>
+                        <View>
+                         <Text style={stylemovie.titleCart}>{item.title}</Text>
+                       </View>
+                      </View>
+                     </View>
+                   </TouchableOpacity>
+                   )
+                 })}
+               </View>
+             </View>
          </View>
        </Root>
       </Modal>
@@ -53,48 +71,20 @@ const Heigth = Dimensions.get("screen").height;
 const Width = Dimensions.get("screen").width;
 const playStyle= StyleSheet.create({
     container: {
-        // flex: 1,
         justifyContent: 'center',
         width: Width,
         height: Heigth,
-        backgroundColor:'red'
       },
       video: {
         flex: 1,
         alignSelf: 'center',
         justifyContent: "center",
         alignItems: "center",
-        width: Heigth,
-        height: Width,
-        transform: [
-          { rotateY: "0deg" },
-          { rotateZ: "90deg" }
-        ],
-        flex: 1,
-        // marginRight: 330,
-        // marginTop: 10,
+        width: Width,
+        height: Heigth,
       },
- 
       conTitle: {
-        //  backgroundColor: "red",
-         width: Width,
-                 
+         width: Width,      
       },
   
-      btnPlay: {
-        width: 100,
-          backgroundColor: "red",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: 5,
-          marginVertical: 10,
-          borderRadius: 5,
-          transform: [
-            { rotateY: "0deg" },
-            { rotateZ: "90deg" }
-          ],
-      },
-      btnPlayText: {
-          color: "#FFFF"
-      }
 })
